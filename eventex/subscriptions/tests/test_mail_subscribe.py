ab040fad362a9f7618d7d54094a65b0bba271524
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2019-12-23 01:06:16 -0300

    Refatora geral

---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,38 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Adriano Ferrari', cpf='13650314894',
+                    email='adriano-ferrari@hotmail.com', phone='11-996-040-987')
+        self.resp = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventex.com.br', 'adriano-ferrari@hotmail.com']
+
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = [
+            'Adriano Ferrari',
+            '13650314894',
+            'adriano-ferrari@hotmail.com',
+            '11-996-040-987',
+        ]
+
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
