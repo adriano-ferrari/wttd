0a32d8004f9cdb44e239d222bb26c69d780b0552
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-01-12 22:33:35 -0300

    Course como MTI

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-
 from eventex.core.managers import PeriodManager
 from eventex.core.models import Talk, Course
 
@@ -40,6 +39,9 @@ class TalkModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Título da Palestra', str(self.talk))
+
+    def test_ordering(self):
+        self.assertListEqual(['start'], Talk._meta.ordering)
 
 
 class PeriodManagerTest(TestCase):
@@ -87,3 +89,6 @@ class CourseModelTest(TestCase):
 
     def test_manager(self):
         self.assertIsInstance(Course.objects, PeriodManager)
+
+    def test_ordering(self):
+        self.assertListEqual(['start'], Course._meta.ordering)
